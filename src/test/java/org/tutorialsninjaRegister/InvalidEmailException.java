4cd653ec05122c5ce7bf20a2c09175b2380d048d
SmitaBidwaiD <smitabidwai2509@gmail.com>
2025-03-31 21:11:37 +0530

    TC 10

---
+++ src/test/java/org/tutorialsninjaRegister/InvalidEmailException.java
@@ -0,0 +1,10 @@
+package org.tutorialsninjaRegister;
+
+public class InvalidEmailException extends Exception {
+
+    public InvalidEmailException(String message){
+        super(message);
+    }
+
+
+}
